99ade2b0713b20ee96d5fa56a1a4a700a352747c
JiButang <842365090@qq.com>
2022-09-17 15:25:27 +0800

    pom文件配置、SpringBoot配置文件

--- src/main/java/com/newemployee/App.java
+++ src/main/java/com/newemployee/App.java
@@ -1,13 +1,28 @@
 package com.newemployee;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
-public class App 
+@MapperScan("com.newemployee")
+@SpringBootApplication(scanBasePackages = {"com.newemployee"})
+@RestController
+public class App
 {
+    @RequestMapping("/test")
+    public String test(){
+        return "Hello World";
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
 }
