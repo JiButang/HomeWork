99f015c325b9933d06cc98a922bf458c7ca68c8a
JiButang <842365090@qq.com>
2022-09-18 09:26:13 +0800

    商品分类等级

---
+++ src/main/java/com/newemployee/vo/ThirdLevelCategoryVO.java
@@ -0,0 +1,19 @@
+package com.newemployee.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class ThirdLevelCategoryVO implements Serializable {
+
+    //当前三级分类id
+    private Long categoryId;
+
+    //当前分类级别
+    private Byte categoryLevel;
+
+    //当前三级分类名称
+    private String categoryName;
+}
