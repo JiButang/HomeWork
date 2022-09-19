9637763455f9def166282da8536b7c56dd6f5bdc
JiButang <842365090@qq.com>
2022-09-19 12:53:57 +0800

    秒杀时间测试

--- src/test/java/com/newemployee/AppTest.java
+++ src/test/java/com/newemployee/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 /**
  * Unit test for simple App.
  */
@@ -15,6 +17,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
+        System.out.println(new Date());
         assertTrue( true );
     }
 }
